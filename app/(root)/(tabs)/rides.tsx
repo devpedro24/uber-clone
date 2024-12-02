7209464de78051bb83ae6a5240d054f6723bd0cd
Pedro Ortiz <93847425+devpedro24@users.noreply.github.com>
2024-12-02 17:50:34 -0500

    first commit

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Rides = () => {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-gray-100 dark:bg-gray-800">
+      <Text>Rides</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Rides;
